7d20210c322cd7bc5e600ea4868444c74f4108ef
jihye1215 <wlgp1494@naver.com>
2022-03-04 15:28:55 +0900

    react #3

---
+++ 1.basics/ex03/public/index.js
@@ -0,0 +1,6 @@
+import appCss from '.App.css';
+import { App } from './App.js';
+
+document
+    .getElementById('root')
+    .appendChild(App());
--- 1.basics/ex04/src/index.js
+++ 1.basics/ex04/src/index.js
@@ -1,3 +1,6 @@
+// import './App.css';
+import { App } from './App.js';
+
 document
     .getElementById('root')
     .appendChild(App());
