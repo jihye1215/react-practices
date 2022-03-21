bfc3ed6ab3d94363f3a2cdae40a78a2938dcf7fa
jihye1215 <wlgp1494@naver.com>
2022-03-21 09:05:07 +0900

    react practice

---
+++ 2.Component/ex02/src/06/readme.txt
@@ -0,0 +1,10 @@
+Font Awesome
+
+1. 코어 설치
+$ npm i @fortawesome/fontawesome-svg-core
+$ npm i @fortawesome/react-fontawesome
+
+2. icon(light(유료), regular, solid(기본), brand(기업)) 선택 설치
+S npm i @fortawesome/free-solid-svg-icons
+S npm i @fortawesome/free-regular-svg-icons
+S npm i @fortawesome/free-brand-svg-icons
---
+++ 2.Component/ex03/src/02/TitleBar02.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TitleBar02 = () => {
+    const onClickHandler = e => {
+        console.log('TitleBar02 Clicked');
+    }
+
+    return (
+        <h1
+                style={{cursor : 'pointer'}}
+                onClick={ onClickHandler }>
+                ex03 : Function Event Handler(Functional Component)
+        </h1>
+    )
+}
+
+export default TitleBar02
